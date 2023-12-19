18548e143689442bd229ba97e1642ee9b9ed4e7a
lovzay <tfvin24@gmail.com>
2023-12-20 05:17:37 +0800

    set up index file

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const Discord = require ("discord.jks")
+const Discord = require ("discord.js")
 const dotenv = require("dotenv")
 const { REST } = require("@discordjs/rest")
-const { Routes } = require ("@discord-api-types/v9")
+const { Routes } = require ("@discord-api-types/v10")
 const fs = require("fs")
 const { Player } = require ("discord-player")
 
@@ -9,3 +9,66 @@ dotenv.config
 const TOKEN = process.env.TOKEN
 
 const LOAD_SLASH = process.argv[2] == "load"
+
+const CLIENT_ID = "1186164608696864768"
+const GUILD_ID = "1184825380050305074"
+
+const client = new Discord.Client({
+    intents: [
+        "GUILDS",
+        "GUILD_VOICE_STATES"
+    ]
+})
+
+client.slashcommands = new Discord.Collection()
+client.player = new Player(client, {
+    ytdlOptions: {
+        quality: "highestaudio",
+        highWaterMark: 1 << 25
+    }
+})
+
+let commands = []
+const slashFiles = fs.readdirSync("./slash").filter(file => file.endsWith(".js"))
+for (const file of slashFiles) {
+    const slashcmd = require(`./slash/${file}`)
+    client.slashcommands.set(slashcmd.data.name, slashcmd)
+    if (LOAD_SLASH) commands.push(slashcmd.data.toJSON())
+}
+
+if (LOAD_SLASH) {
+    const rest = new REST({ version: "9"}).setToken(TOKEN)
+    console.log("Deploying slash commands")
+    rest.put(Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID), {body: commands})
+    .then(() => {
+        console.log("Successfully Loaded")
+        process.exit(0)
+    })
+    .catch((err) => {
+        if (err){
+        console.log(err)
+        process.exit(1)
+        }
+    })
+}
+
+else {
+    client.on("ready", () => {
+        console.log(`Logged in as ${client.user.tag}`)
+
+    })
+    client.on("interactionCreate", (interaction) => { 
+        async function handleCommand() {
+            if(!interaction.isCommand())
+            return
+
+            const slashcmd = client.slashcommands.get(interaction.commandName)
+            if (!slashcmd) interaction.reply("Not a valid slash command")
+
+            await interaction.deferReply()
+            await slashcmd.run({client, interaction})
+        }
+        handleCommand()
+    })
+    client.login(TOKEN)
+}
